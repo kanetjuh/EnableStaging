6073a9cf81b60d3978f1202a41be084e68cb4f06
Kane <50002439+kanetjuh@users.noreply.github.com>
2023-02-19 02:18:41 +0100

    Update EnableStaging.js

--- dist/EnableStaging.js
+++ dist/EnableStaging.js
@@ -26,7 +26,7 @@ const l = {
             m.name.includes("Experiment") &&
               m.actionHandler({
                 serializedExperimentStore: t.getSerializedState(),
-                user: { flags: [0]},
+                user: { flags: 0},
               });
           });
     };
